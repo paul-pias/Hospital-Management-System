df37fe26c91dd51666b6589e89c51acb0faddaeb
Pias Paul <paul.pias@northsouth.edu>
2018-07-01 20:12:12 +0600

    Database created for booking in home page

--- humanbeat.com/func.php
+++ humanbeat.com/func.php
@@ -21,8 +21,24 @@ if(isset($_POST['pat_submit']))
   if($result)
   {
     echo "<script>alert ('Successfully Registered')</script>";
+    echo "<script>window.open('index.php','_self')</script>";
   }
 
+}
+
+if(isset($_POST['appointment']))
+{
+  $patient_name=$_POST['patient_name'];
+  $departments=$_POST['departments'];
+  $date=$_POST['date'];
+  $time=$_POST['time'];
+
+  $booking = " insert into appointment_booking(patient_name,departments,date,time)values ('$patient_name','$departments','$date','$time')";
+  $booking_result = mysqli_query($con,$booking);
+  if($booking_result)
+  {
+    echo "<script>window.open('index.php','_self')</script>";
+  }
 }
 
  ?>
