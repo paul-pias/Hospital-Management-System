faee507a3971e04627caefdeb14dd4bb437631c6
Unknown <unknown@example.com>
2018-07-02 11:18:09 +0600

    FOOTER linked

--- humanbeat.com/about.php
+++ humanbeat.com/about.php
@@ -287,71 +287,9 @@
 <!-- //main -->
 
 <!-- Footer -->
-<div class="footer w3ls">
-	<div class="container">
-		<div class="footer-main">
-			<div class="footer-top">
-				<div class="col-md-4 services-grids">
-					<h3>About Us</h3>
-					<ul>
-						<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a href="single.html">Lorem ipsum dolor sit amet, consectetur.</a></li>
-						<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a href="single.html">do eiusmod tempor incididunt ut labore.</a></li>
-						<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a href="single.html">et dolore magna aliqua.</a></li>
-						<li><i class="fa fa-angle-double-right" aria-hidden="true"></i><a href="single.html">onec dut quam lscel quam erisque.</a></li>
-					</ul>
-				</div>
-				<div class="col-md-4 ftr-grid">
-					<div class="logo-fo">
-						<div class="logo-w3l">
-							<i class="fa fa-stethoscope" aria-hidden="true"></i>
-						</div>
-						<h2><a href="index.html">HUMAN BEAT<span>Best care For you</span></a></h2>
-					</div>
-				</div>
-				<div class="col-md-4 ftr-grid fg2">
-					<h3>Our Address</h3>
-					<div class="ftr-address">
-						<div class="local">
-							<i class="fa fa-map-marker" aria-hidden="true"></i>
-						</div>
-						<div class="ftr-text">
-							<p>Lorem ipsum dolor sit amet.</p>
-						</div>
-						<div class="clearfix"> </div>
-					</div>
-					<div class="ftr-address">
-						<div class="local">
-							<i class="fa fa-phone" aria-hidden="true"></i>
-						</div>
-						<div class="ftr-text">
-							<p>+88 02 154 8176</p>
-						</div>
-						<div class="clearfix"> </div>
-					</div>
-					<div class="ftr-address">
-						<div class="local">
-							<i class="fa fa-envelope" aria-hidden="true"></i>
-						</div>
-						<div class="ftr-text">
-							<p><a href="mailto:info@example.com">info@example.com</a></p>
-						</div>
-						<div class="clearfix"> </div>
-					</div>
-				</div>
-				<div class="clearfix"> </div>
-			</div>
-				<div class="right-w3l">
-					<ul class="top-links">
-						<li class="get-w3"><a href="mail.html"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i>Get In Touch</a></li>
-						<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-						<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-						<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-					</ul>
-				</div>
-			
-		</div>
-	</div>
-</div>
+<DIV>
+	<?php require_once('inc/footer.php') ?>
+</DIV>
 <!-- //Footer -->
 
 <!-- js-scripts -->
