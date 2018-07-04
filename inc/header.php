f0665d109d3268aa57177614455099b718bd3d66
Md Jahidul Hossain <jahidcsensu@gmail.com>
2018-07-04 23:56:56 +0600

    add live chat code in header file. Don't Change it. http://www.humanbeat.ml/ check website for better understanding. thank-you.

--- inc/header.php
+++ inc/header.php
@@ -23,4 +23,13 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 <link href="//fonts.googleapis.com/css?family=Convergence" rel="stylesheet">
 <link href="//fonts.googleapis.com/css?family=ABeeZee:400,400i" rel="stylesheet">
 <!-- //online-fonts -->
+<!--Start of Zendesk Chat Script-->
+<script type="text/javascript">
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+$.src="https://v2.zopim.com/?5pWPe5M9slFevbCGSxEAARQWj5wL1bXc";z.t=+new Date;$.
+type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+</script>
+<!--End of Zendesk Chat Script-->
 </head>
