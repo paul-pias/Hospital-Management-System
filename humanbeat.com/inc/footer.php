5e0057aeb56d890ed2c6c0afdf4a5b05af2e28a4
Pias Paul <paul.pias@northsouth.edu>
2018-08-02 05:26:12 +0600

    footer updated

--- humanbeat.com/inc/footer.php
+++ humanbeat.com/inc/footer.php
@@ -70,39 +70,38 @@
 <footer class="footer">
 <div class="container-fluid">
 
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <style>
-        /* Set the size of the div element that contains the map */
-        #map {
-          height: 150px;  /* The height is 400 pixels */
-          width: 100%;  /* The width is the width of the web page */
-         }
-      </style>
-    </head>
-    <body>
-      <h3>Here to find us</h3>
-      <!--The div element for the map -->
-      <div id="map"></div>
-      <script>
-  // Initialize and add the map
-  function initMap() {
-    // The location of Uluru
-		// The location of NSU
-	  var nsu = {lat: 23.8151, lng: 90.4255};
-	  var location_info = " North South University \n Plot 15, Block B, Bashundhara \n Bangladesh ";
-	  // The map, centered at NSU
-	  var map = new google.maps.Map(
-	      document.getElementById('map'), {zoom: 18, center: nsu});
-	  // The marker, positioned at NSU
-	  var marker = new google.maps.Marker({position: nsu, map: map,title:location_info});
-  }
-      </script>
-      <script async defer
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDDLbp9ciApf2bMJNuRT_8Ki3snPSXRmME&callback=initMap">
-      </script>
-    </body>
-  </html>
+	<!DOCTYPE html>
+   <html>
+     <head>
+       <style>
+         /* Set the size of the div element that contains the map */
+         #map {
+           height: 150px;  /* The height is 400 pixels */
+           width: 100%;  /* The width is the width of the web page */
+          }
+       </style>
+     </head>
+     <body>
+       <h3>Here to find us</h3>
+       <!--The div element for the map -->
+       <div id="map"></div>
+       <script>
+   // Initialize and add the map
+   function initMap() {
+ 		// The location of NSU
+ 	  var nsu = {lat: 23.8151, lng: 90.4255};
+ 	  var location_info = " North South University \n Plot 15, Block B, Bashundhara \n Bangladesh ";
+ 	  // The map, centered at NSU
+ 	  var map = new google.maps.Map(
+ 	      document.getElementById('map'), {zoom: 18, center: nsu});
+ 	  // The marker, positioned at NSU
+ 	  var marker = new google.maps.Marker({position: nsu, map: map,title:location_info});
+   }
+       </script>
+       <script async defer
+       src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDDLbp9ciApf2bMJNuRT_8Ki3snPSXRmME&callback=initMap">
+       </script>
+     </body>
+   </html>
 
 </footer>
